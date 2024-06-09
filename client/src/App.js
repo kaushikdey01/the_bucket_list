80c6608d4841a5cbf73d4858c7d862f73a9820ad
lazarus01eaking <k2dey20@gmail.com>
2024-06-09 20:31:53 +0530

    made slight changes in app.js created form.js, Posts.js, Post.js with their respective style.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Container, AppBar, Typography, Grow, Grid} from '@mui/material'
+import bucket from "./assets/images/bucket.jpg"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth = "lg">
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2"align="center">
+          the bucket list 
+        </Typography>
+        <img src={bucket} alt="bucket_list_icon" height="60" /> 
+      </AppBar>
+    </Container>
   );
 }
+
+
 
 export default App;
